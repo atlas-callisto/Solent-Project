a82f8966349b4c5872ec4dfb3b0671ad8cb16047
Dylan Karsak <dylankarsak@yahoo.co.uk>
2021-02-15 20:44:18 +0000

    Created Spike.CS

---
+++ Unity Project/Solent Group Project/Assets/Scripts/Spike.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    public float timer;
+    public float damageTickRate;
+    public int damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        IDamageable iDamageableObj;
+        iDamageableObj = collision.gameObject.GetComponent<IDamageable>();
+        if (iDamageableObj != null)
+        {
+            if (timer <= damageTickRate)
+            {
+                timer += Time.deltaTime;
+                if (timer >= damageTickRate)
+                {
+                    timer = 0;
+                    iDamageableObj.TakeDamage(damage);
+                }
+            }
+            print("Collision");
+        }
+    }
+
+}
