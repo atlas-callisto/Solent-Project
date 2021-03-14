0c2d4c19ac0496789683f797d4ae01506dd2e34f
Jubin <jubsi_713@yahoo.com>
2021-03-14 00:51:17 +0000

    Passable Platform Created
    Here's a passable platform, the player can stand on it and jump to it from below. As well as the player can press down button to jump down from it.

---
+++ Unity Project/Solent Group Project/Assets/Scripts/PassablePlatform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassablePlatform : MonoBehaviour
+{
+    private PlatformEffector2D myPlatformEffector2D;
+    void Start()
+    {
+        myPlatformEffector2D = GetComponent<PlatformEffector2D>();
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if(Input.GetKey(KeyCode.S))
+            {
+                myPlatformEffector2D.rotationalOffset = 180;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            myPlatformEffector2D.rotationalOffset = 0;
+        }
+    }
+
+}
