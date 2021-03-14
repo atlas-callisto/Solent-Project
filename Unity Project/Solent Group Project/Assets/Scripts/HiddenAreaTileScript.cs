f52bc6e74e5fa3a06d9e01196e5c4435781703e0
ThatGuyVyse <78847780+ThatGuyVyse@users.noreply.github.com>
2021-03-14 22:06:48 +0000

    Hub area update 1, expanded and added hidden area script

---
+++ Unity Project/Solent Group Project/Assets/Scripts/HiddenAreaTileScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class HiddenAreaTileScript : MonoBehaviour
+{
+    public GameObject HubHiddenArea;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == ("Player"))
+        {
+            HubHiddenArea.GetComponent<TilemapRenderer>().enabled = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        HubHiddenArea.GetComponent<TilemapRenderer>().enabled = true;
+    }
+}
