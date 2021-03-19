b3bcab9696b734ffecda8e68e1f253399e6eeedc
Dylan Karsak <dylankarsak@yahoo.co.uk>
2021-03-19 21:27:49 +0000

    Buffed damage cooldown on the player from 0.5 -> 1.5 seconds
    Player would just take hits so quickly. This system was already in but just needed to be more generous. @Nicks123111

--- Unity Project/Solent Group Project/Assets/Scripts/Player/Player.cs
+++ Unity Project/Solent Group Project/Assets/Scripts/Player/Player.cs
@@ -51,7 +51,7 @@ public class Player : MonoBehaviour, IDamageable
     internal bool doubleJumpSkillAcquired = false; // enable double jump after skill is unlocked??? for later use
     private bool playerCanTakeDmg = true;
     private float timer = 0.2f; //timer to enable hitbox duration to match attack animation
-    private float playerInvunerabletimer = 0.5f; // timer to stop player playing from getting damage after taking a hit
+    private float playerInvunerabletimer = 1.5f; // timer to stop player playing from getting damage after taking a hit
 
     //Comp Ref
     private Rigidbody2D myRB;
