1176271864012e47a4c9f422f13d717a1a960ad0
Dylan Karsak <dylankarsak@yahoo.co.uk>
2021-03-19 20:53:31 +0000

    Made Pickup Hover Script

---
+++ Unity Project/Solent Group Project/Assets/Scripts/UI and Manager/PickupHovering.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupHovering : MonoBehaviour
+{
+    public float VerticalMovementSpeed;
+
+    private float TravelledDistance;
+    public float MaxTravelDistance;
+
+    public bool IsReturning;
+
+    void Update()
+    {
+        // Up and down wobbling
+        if (TravelledDistance <= MaxTravelDistance && !IsReturning)
+        {
+            transform.Translate(0, VerticalMovementSpeed * Time.deltaTime, 0);
+            TravelledDistance += VerticalMovementSpeed;
+        }
+        else if (TravelledDistance >= MaxTravelDistance)
+        {
+            IsReturning = true;
+        }
+        if (IsReturning)
+        {
+            transform.Translate(0, -VerticalMovementSpeed * Time.deltaTime, 0);
+            TravelledDistance += -VerticalMovementSpeed;
+
+            if (TravelledDistance <= -MaxTravelDistance)
+            {
+                IsReturning = false;
+            }
+        }
+    }
+}
