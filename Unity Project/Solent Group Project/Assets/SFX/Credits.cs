18f49f3698ba8a9f6c9181d70086ce98f442750e
Dylan Karsak <dylankarsak@yahoo.co.uk>
2021-03-27 21:58:37 +0000

    Added credits text file for SFX
    Please reference any sound files you've sourced in this text file

---
+++ Unity Project/Solent Group Project/Assets/SFX/Credits.cs
@@ -0,0 +1,6 @@
+﻿public class Credits
+{
+    //PLEASE CREDIT ANYTHING YOU'VE USED IN HERE SO WE DONT GET DONE FOR ACADEMIC INTEGRITY
+
+    // Arrow Trap Sound: https://opengameart.org/content/bow-arrow-shot
+}
