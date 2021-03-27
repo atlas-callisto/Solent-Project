111caec4433a15c3e63fbefbd72b57b51f6cb2df
Dylan Karsak <dylankarsak@yahoo.co.uk>
2021-03-27 22:06:11 +0000

    Created arrow trap
    Arrow trap also has been placed in catacombs for testing

---
+++ Unity Project/Solent Group Project/Assets/Scripts/Hazards/ArrowTrap.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowTrap : MonoBehaviour
+{
+    [Header("References")]
+    public AudioSource ShootSound;
+    public GameObject Arrow;
+
+    [Header("Config")]
+    // Use these to tune the spawn position of the arrow
+    public float xArrowSpawnOffset;
+    public float yArrowSpawnOffset;
+    public int Ammo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // This is a failsafe incase you forget to assign values in the inspector
+        if(Ammo <= 0)
+        {
+            Ammo = 3;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (Ammo > 0)
+            {
+                ShootSound.Play();
+
+                Instantiate(Arrow, transform.position + new Vector3(xArrowSpawnOffset, yArrowSpawnOffset, 0), transform.rotation);
+                Ammo--;
+            }
+        }
+    }
+}
