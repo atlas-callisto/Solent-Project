5b4d382aa7c957e7332f90f801f23b16e54cf06c
Dylan Karsak <dylankarsak@yahoo.co.uk>
2021-03-29 16:51:58 +0100

    Updated Tutorial Hints

---
+++ Unity Project/Solent Group Project/Assets/JumpHintUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JumpHintUI : MonoBehaviour
+{
+    public GameManager GM;
+    public Text JumpHint;
+
+    void Start()
+    {
+        JumpHint.enabled = false;
+    }
+
+    void Update()
+    {
+        if (GM.airTreaders)
+        {
+            JumpHint.enabled = true;
+        }
+        else
+        {
+            JumpHint.enabled = false;
+        }
+    }
+}
