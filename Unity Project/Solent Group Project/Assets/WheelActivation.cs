5d7a4c19b3c7242feb274c1a4e61a56ba1e8539f
Shaun <75041428+Duke-of-Flawless@users.noreply.github.com>
2021-03-30 14:55:23 +0100

    Wheel anim(needs interact code)

---
+++ Unity Project/Solent Group Project/Assets/WheelActivation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelActivation : MonoBehaviour
+{
+    private Animator myAnimator;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            myAnimator.SetTrigger("Interact");
+        }
+        
+    }
+}
