b3547cd4b813762b24ccb7f998c3b508662f3e8b
Inhcredible <57454655+Inhcredible@users.noreply.github.com>
2021-03-31 05:30:03 +0100

    SFX and Background update
    various SFX has been implimented to enemies, player and wolf. as well as background music for all scenes

--- Unity Project/Solent Group Project/Assets/SFX/Credits.cs
+++ Unity Project/Solent Group Project/Assets/SFX/Credits.cs
@@ -3,4 +3,24 @@
     //PLEASE CREDIT ANYTHING YOU'VE USED IN HERE SO WE DONT GET DONE FOR ACADEMIC INTEGRITY
 
     // Arrow Trap Sound: https://opengameart.org/content/bow-arrow-shot
+
+    // Spider Death Sound: https://freesound.org/s/505185/
+
+    // Boss Battle music: https://freesound.org/s/353773/
+
+    // wolf howl sound: https://freesound.org/s/268504/
+
+    // Player jump sound: https://freesound.org/s/456370/
+
+    // Hub ambience: https://freesound.org/people/PatrickLieberkind/sounds/244961/
+
+    // player bow sound: https://freesound.org/s/321555/
+
+    // Cathederal ambience: https://freesound.org/s/391485/
+
+    // Skeleton Death sound: https://freesound.org/s/202091/
+
+    // Bat death sound: https://freesound.org/s/257929/
+
+    // Templar Knight sounds: https://freesound.org/s/474651/
 }
