6fd5a78b8b489f33a1b241613cedee0df32b0370
Dylan Karsak <dylankarsak@yahoo.co.uk>
2021-04-21 17:51:52 +0100

    Removed Cursor from Levels (Read Desc)
    The cursor is now hidden from all the levels, I did this by attaching a cursor hiding script to the player itself. So if you want the cursor to be visible when the pause menu is up, edit the cursor hider script I attached to the player.

---
+++ Unity Project/Solent Group Project/Assets/Scripts/CursorHider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorHider : MonoBehaviour
+{
+    // Use this to disable the cursor in the scene this is attached to
+    void Start()
+    {
+        //Set Cursor to not be visible
+        Cursor.visible = false;
+    }
+}
