77ee9f0d862b46f14cf9f7cb4fdebbb3d48f7e67
Shaun <75041428+Duke-of-Flawless@users.noreply.github.com>
2021-04-25 20:13:42 +0100

    Add potions falling to the ground

--- Unity Project/Solent Group Project/Assets/Scripts/Item/HealthPotion.cs
+++ Unity Project/Solent Group Project/Assets/Scripts/Item/HealthPotion.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 public class HealthPotion : MonoBehaviour
 {
     [SerializeField] int healingAmount = 5;
+    private bool isTouchingDown;
+    public Rigidbody2D potionrb;
+    [SerializeField] float groundCheckRadius;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Transform groundCheckDown;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+        if (collision.tag == "Player")
         {
             Player.currentHealth = Mathf.Clamp(Player.currentHealth + healingAmount, 0, Player.maxHealth);
             Destroy(this.gameObject);
         }
     }
+
+    private void Update()
+    {
+        isTouchingDown = Physics2D.OverlapCircle(groundCheckDown.position, groundCheckRadius, groundLayer);
+        if (isTouchingDown)
+        {
+            potionrb.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        }
+    }
+
 }
--- Unity Project/Solent Group Project/Assets/Scripts/Item/ManaPotion.cs
+++ Unity Project/Solent Group Project/Assets/Scripts/Item/ManaPotion.cs
@@ -5,12 +5,27 @@ using UnityEngine;
 public class ManaPotion : MonoBehaviour
 {
     [SerializeField] int manaAmount = 5;
+    private bool isTouchingDown;
+    public Rigidbody2D potionrb;
+    [SerializeField] float groundCheckRadius;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Transform groundCheckDown;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
             Player.currentWolfBar = Mathf.Clamp(Player.currentWolfBar + manaAmount, 0, Player.maxWolfBar);
             Destroy(this.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        isTouchingDown = Physics2D.OverlapCircle(groundCheckDown.position, groundCheckRadius, groundLayer);
+        if (isTouchingDown)
+        {
+            potionrb.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
         }
     }
 }
