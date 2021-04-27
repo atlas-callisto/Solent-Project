d8b4c016c14f1dc3491f93142c7163926bb15085
Dylan Karsak <dylankarsak@yahoo.co.uk>
2021-04-28 00:56:29 +0100

    Fixed stupid bug with Missing Monocle Hint AGAIN

--- Unity Project/Solent Group Project/Assets/Scripts/UI and Manager/MonacleHintText.cs
+++ Unity Project/Solent Group Project/Assets/Scripts/UI and Manager/MonacleHintText.cs
@@ -6,18 +6,10 @@ public class MonacleHintText : MonoBehaviour
 {
     public GameManager GM;
 
-    // I had to add both enter & exit, because it wasn't always being detected by OnEnter
-    private void OnTriggerEnter2D(Collider2D collision)
+    // I have to do this per update because it's just fucking dumb
+    private void LateUpdate()
     {
-        if (GM.moonsEyeMonacle)
-        {
-            gameObject.SetActive(false);
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (GM.moonsEyeMonacle)
+        if(GM.moonsEyeMonacle)
         {
             gameObject.SetActive(false);
         }
