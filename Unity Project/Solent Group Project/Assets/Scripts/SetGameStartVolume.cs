a755bd5282f7edd2c15eed27affbb7832b977137
Dylan Karsak <dylankarsak@yahoo.co.uk>
2021-04-27 23:36:28 +0100

    Added Lighting System & Organised Files
    Added Universal Render Pipeline and converted the catacombs scene to use lighting, so we can add lights.

---
+++ Unity Project/Solent Group Project/Assets/Scripts/SetGameStartVolume.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetGameStartVolume : MonoBehaviour
+{
+    private static bool doOnce = true;
+    public float defaultSFXVolumeWhenGameStarts = 0.5f;
+    public float defaultMusicVolumeWhenGameStarts = 0.5f;
+
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Update()
+    {
+        if(doOnce)
+        {
+            doOnce = false;
+            GameManager.myGameManager.UpdatePlayerPrefs(defaultSFXVolumeWhenGameStarts, defaultMusicVolumeWhenGameStarts);
+        }        
+    }
+}
---
+++ Unity Project/Solent Group Project/Assets/Scripts/TransformHintUX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransformHintUX : MonoBehaviour
+{
+    public GameManager GM;
+    public Text HintText;
+
+    void Start()
+    {
+        HintText.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            HintText.enabled = true;
+        }
+    }
+}
