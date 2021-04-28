ec33c72451227ac91433aab6843837de310e89cf
Dylan Karsak <dylankarsak@yahoo.co.uk>
2021-04-28 01:03:21 +0100

    Final Attempt at Fixing Wrong Way Hint

--- Unity Project/Solent Group Project/Assets/Scripts/UI and Manager/MonacleHintText.cs
+++ Unity Project/Solent Group Project/Assets/Scripts/UI and Manager/MonacleHintText.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MonacleHintText : MonoBehaviour
 {
     public GameManager GM;
+    public Text MyText;
 
     // I have to do this per update because it's just fucking dumb
-    private void LateUpdate()
+    void Update()
     {
         if(GM.moonsEyeMonacle)
         {
-            gameObject.SetActive(false);
+            MyText.enabled = false;
         }
     }
 }
